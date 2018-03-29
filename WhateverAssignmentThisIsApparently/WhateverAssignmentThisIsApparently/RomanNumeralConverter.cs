a5d69153b37170154597272828513fc0a116eebd
Samantha15perez <Samantha15perez@gmail.com>
2018-03-28 21:42:36 -0400

    error handling tweaked

--- WhateverAssignmentThisIsApparently/WhateverAssignmentThisIsApparently/RomanNumeralConverter.cs
+++ WhateverAssignmentThisIsApparently/WhateverAssignmentThisIsApparently/RomanNumeralConverter.cs
@@ -8,6 +8,7 @@ namespace WhateverAssignmentThisIsApparently
 {
     class RomanNumeralConverter
     {
+        //this part covers Roman numeral -> number
         public static Dictionary<string, int> RomanMap = new Dictionary<string, int>()
     {
          {"I", 1},
@@ -29,6 +30,9 @@ namespace WhateverAssignmentThisIsApparently
         public static int RomanToInteger(string roman)
          {
 
+            try
+            {
+
 
                int number = 0;
               for (int i = 0; i < roman.Length; i++)
@@ -42,11 +46,18 @@ namespace WhateverAssignmentThisIsApparently
                       number += RomanMap[roman[i].ToString()];
                   }
               }
-              return number; 
+              return number;
+
+            }
+            catch (Exception ex)
+            {
+                throw(ex);
+            }
     }
     }
     class ArabicToRomanConversion
     {
+        // everything past here covers number -> roman numeral
        public static Dictionary<string, int> RomanMap = new Dictionary<string, int>()
     {
          {"I", 1},
@@ -65,86 +76,93 @@ namespace WhateverAssignmentThisIsApparently
     };
         public static string ArabicToRoman(int roman)
         {
-            int inputValue = roman;
-            string OutputValue = "";
+            try
+            {
 
-            if (inputValue / 1000 >= 1)
-            {
-                inputValue = (inputValue - 1000);
-                OutputValue = "M";
-            }
-            while (inputValue / 900 != 0)
-            {
-                inputValue = (inputValue - 900);
-                OutputValue = (OutputValue + "CM");
-            }
+
+
+                int inputValue = roman;
+                string OutputValue = "";
+
+                if (inputValue / 1000 >= 1)
+                {
+                    inputValue = (inputValue - 1000);
+                    OutputValue = "M";
+                }
+                while (inputValue / 900 != 0)
+                {
+                    inputValue = (inputValue - 900);
+                    OutputValue = (OutputValue + "CM");
+                }
                 while (inputValue / 500 != 0)
                 {
                     inputValue = (inputValue - 500);
                     OutputValue = (OutputValue + "D");
-                }         
+                }
                 while (inputValue / 400 != 0)
-            {
-                inputValue = (inputValue - 400);
-                OutputValue = (OutputValue + "CD");
-            }
+                {
+                    inputValue = (inputValue - 400);
+                    OutputValue = (OutputValue + "CD");
+                }
                 while (inputValue / 100 != 0)
                 {
                     inputValue = (inputValue - 100);
                     OutputValue = (OutputValue + "C");
                 }
                 while (inputValue / 90 != 0)
-            {
-                inputValue = (inputValue - 90);
-                OutputValue = (OutputValue + "XC");
-            }
+                {
+                    inputValue = (inputValue - 90);
+                    OutputValue = (OutputValue + "XC");
+                }
                 while (inputValue / 50 != 0)
                 {
                     inputValue = (inputValue - 50);
                     OutputValue = (OutputValue + "L");
                 }
                 while (inputValue / 40 != 0)
-            {
-                inputValue = (inputValue - 40);
-                OutputValue = (OutputValue + "XL");
-            }
+                {
+                    inputValue = (inputValue - 40);
+                    OutputValue = (OutputValue + "XL");
+                }
                 while (inputValue / 10 != 0)
                 {
-                //if (inputValue > 15)
-               // {
-
-                //}
+                   
                     inputValue = (inputValue - 10);
                     OutputValue = (OutputValue + "X");
                 }
                 while (inputValue / 5 != 0)
                 {
-                if (inputValue == 9)
-                {
-                    inputValue = (inputValue - 9);
-                    OutputValue = (OutputValue + "IX");
-                }
-                else
-                {                
-                    inputValue = (inputValue - 5);
-                    OutputValue = (OutputValue + "V");
-                }
+                    if (inputValue == 9)
+                    {
+                        inputValue = (inputValue - 9);
+                        OutputValue = (OutputValue + "IX");
+                    }
+                    else
+                    {
+                        inputValue = (inputValue - 5);
+                        OutputValue = (OutputValue + "V");
+                    }
                 }
                 while (inputValue / 1 != 0)
                 {
-                if (inputValue / 1 > 3)
-                {
-                    inputValue = (inputValue - 4);
-                    OutputValue = (OutputValue + "IV");
+                    if (inputValue / 1 > 3)
+                    {
+                        inputValue = (inputValue - 4);
+                        OutputValue = (OutputValue + "IV");
+                    }
+                    else
+                    {
+                        inputValue = (inputValue - 1);
+                        OutputValue = (OutputValue + "I");
+                    }
                 }
-                else
-                {
-                    inputValue = (inputValue - 1);
-                    OutputValue = (OutputValue + "I");
-                }
-                }
-            return OutputValue;
+                return OutputValue;
             }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
                     
         }
     }
